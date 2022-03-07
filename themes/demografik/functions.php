015f7d6d638c03da5d0314dd1662951efcbc1de7
bakhtiyartayirov <bakhtiyartayirov@gmail.com>
2022-03-07 17:05:53 +0500

    function add pll register string

--- themes/demografik/functions.php
+++ themes/demografik/functions.php
@@ -211,3 +211,12 @@ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' ); 
 remove_action( 'wp_print_styles', 'print_emoji_styles' ); 
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+
+add_action('init', function() {
+	pll_register_string('demografik', 'Digital library');
+	pll_register_string('demografik', 'Search');
+	pll_register_string('demografik', 'Categories');
+	pll_register_string('demografik', 'Search Results for:');
+
+  });
