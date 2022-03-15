278bd9cb8b591e49c19a33d4a796fe8b98c8307f
bakhtiyartayirov <bakhtiyartayirov@gmail.com>
2022-03-15 15:35:49 +0500

    update infographics.php

--- themes/demografik/template-parts/infographics.php
+++ themes/demografik/template-parts/infographics.php
@@ -1,12 +1,26 @@
 <?php
 
-$postslistinfografika = get_posts( [
+$postslistinfografika_on_top = get_posts( [
 	'posts_per_page' => 100,
 	'post_type' => 'infographics',
 	'infographic-cat' => 'infografika',
 	'meta_key' => 'sort_post',
+	'meta_value' => true,
     'orderby' => 'meta_value_num',
     'order' => 'ASC',	
+] );
+$exlude_infografika = [];
+foreach( $postslistinfografika_on_top as $pst ) :
+	setup_postdata($pst);
+	array_push($exlude_infografika, $pst->ID);
+endforeach;
+$postslistinfografika = get_posts( [
+	'posts_per_page' => 100,
+	'post_type' => 'infographics',
+	'infographic-cat' => 'infografika',
+	'exclude' => $exlude_infografika,
+    'orderby' => 'date',
+    'order' => 'DESC',	
 ] ); 
 $postslistrasm = get_posts( [
 	'posts_per_page' => 100,
@@ -98,55 +112,103 @@ foreach( $postslistdocument as $post ){
 				</div>
 				<div class="custom-grid-1-4 photo-gallery">
 					<?php
-					foreach( $postslistinfografika as $post ):
-						setup_postdata($post);
-						if(has_post_thumbnail()):
-							?>
-							<div>
-							 <?php 
-									if( '' !== $post->post_content ) { ?>
-										<div class="video-21-9-rate-wrapepr img_url ">
-											<a data-toggle="modal" data-target="#modal-<?php the_ID() ?>" style="display: flex;justify-content: center; position: relative;">
-												<img src="<?php echo get_template_directory_uri() . '/assets/images/play_icon.png' ?>" class="play_icon">
-												<img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid">
-											</a>
-											<?php the_field('video_link'); ?>
-										</div>
-										<h5 class="custom-post-title"><?php the_title(); ?></h5>
-									<?php }else{ ?>
-										<div class="video-21-9-rate-wrapepr img_url ">
-										<a class="href_img" href="<?php echo get_the_post_thumbnail_url(); ?>" data-title="<?php the_title(); ?>" data-gallery="infographics"
-											data-toggle="lightbox" data-target=".bd-example-modal-lg">
-											<img src="<?php echo the_post_thumbnail_url() ?>" class="video-21-9-rate-images">
-										</a>
-										</div>
-										<h5 class="custom-post-title"><?php the_title(); ?></h5>
-								<?php	}
+					if (!empty($postslistinfografika_on_top)) :
+						foreach( $postslistinfografika_on_top as $post ):
+							setup_postdata($post);
+							if(has_post_thumbnail()):
 								?>
-							</div>
-								<div class="modal fade custom-modal" id="modal-<?php the_ID() ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-								 <div class="modal-dialog modal-dialog-centered" role="document">
-									<div class="modal-content">
-										<div class="modal-header">
-											
-											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-											<span aria-hidden="true">&times;</span>
-											</button>
-											<h5 class="modal-title" id="exampleModalLongTitle"><?php the_title(); ?></h5>
-										</div>
-										<div class="modal-body">
-											<?php the_content(); ?>
+								<div>
+								 <?php 
+										if( '' !== $post->post_content ) { ?>
+											<div class="video-21-9-rate-wrapepr img_url ">
+												<a data-toggle="modal" data-target="#modal-<?php the_ID() ?>" style="display: flex;justify-content: center; position: relative;">
+													<img src="<?php echo get_template_directory_uri() . '/assets/images/play_icon.png' ?>" class="play_icon">
+													<img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid">
+												</a>
+												<?php the_field('video_link'); ?>
+											</div>
+											<h5 class="custom-post-title"><?php the_title(); ?></h5>
+										<?php }else{ ?>
+											<div class="video-21-9-rate-wrapepr img_url ">
+											<a class="href_img" href="<?php echo get_the_post_thumbnail_url(); ?>" data-title="<?php the_title(); ?>" data-gallery="infographics"
+												data-toggle="lightbox" data-target=".bd-example-modal-lg">
+												<img src="<?php echo the_post_thumbnail_url() ?>" class="video-21-9-rate-images">
+											</a>
+											</div>
+											<h5 class="custom-post-title"><?php the_title(); ?></h5>
+									<?php	}
+									?>
+								</div>
+									<div class="modal fade custom-modal" id="modal-<?php the_ID() ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+									 <div class="modal-dialog modal-dialog-centered" role="document">
+										<div class="modal-content">
+											<div class="modal-header">
+												
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+												</button>
+												<h5 class="modal-title" id="exampleModalLongTitle"><?php the_title(); ?></h5>
+											</div>
+											<div class="modal-body">
+												<?php the_content(); ?>
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-						<?php
-						endif;
-					endforeach;
-					wp_reset_postdata();
-
-				
-
+							<?php
+							endif;
+						endforeach;
+						wp_reset_postdata();
+					endif;
+					if (!empty($postslistinfografika)):
+						foreach( $postslistinfografika as $post ):
+							setup_postdata($post);
+							if(has_post_thumbnail()):
+								?>
+								<div>
+								 <?php 
+										if( '' !== $post->post_content ) { ?>
+											<div class="video-21-9-rate-wrapepr img_url ">
+												<a data-toggle="modal" data-target="#modal-<?php the_ID() ?>" style="display: flex;justify-content: center; position: relative;">
+													<img src="<?php echo get_template_directory_uri() . '/assets/images/play_icon.png' ?>" class="play_icon">
+													<img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid">
+												</a>
+												<?php the_field('video_link'); ?>
+											</div>
+											<h5 class="custom-post-title"><?php the_title(); ?></h5>
+										<?php }else{ ?>
+											<div class="video-21-9-rate-wrapepr img_url ">
+											<a class="href_img" href="<?php echo get_the_post_thumbnail_url(); ?>" data-title="<?php the_title(); ?>" data-gallery="infographics"
+												data-toggle="lightbox" data-target=".bd-example-modal-lg">
+												<img src="<?php echo the_post_thumbnail_url() ?>" class="video-21-9-rate-images">
+											</a>
+											</div>
+											<h5 class="custom-post-title"><?php the_title(); ?></h5>
+									<?php	}
+									?>
+								</div>
+									<div class="modal fade custom-modal" id="modal-<?php the_ID() ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+									 <div class="modal-dialog modal-dialog-centered" role="document">
+										<div class="modal-content">
+											<div class="modal-header">
+												
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+												</button>
+												<h5 class="modal-title" id="exampleModalLongTitle"><?php the_title(); ?></h5>
+											</div>
+											<div class="modal-body">
+												<?php the_content(); ?>
+											</div>
+										</div>
+									</div>
+								</div>
+							<?php
+							endif;
+						endforeach;
+						wp_reset_postdata();
+					endif;
+					
 					?>
 				</div>
 			</div>
