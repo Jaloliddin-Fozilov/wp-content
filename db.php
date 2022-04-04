188acf44d7c2262ea99e4a992a6490e77c9fe9d3
bakhtiyart <bakhtiyarkhozha@gmail.com>
2022-04-04 17:41:18 +0500

    infografika sahifasini ozgartirish

---
+++ db.php
@@ -0,0 +1,1 @@
+/opt/lampp/htdocs/demo/wp-content/plugins/query-monitor/wp-content/db.php
--- themes/demografik/page-infografika.php
+++ themes/demografik/page-infografika.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Template Name: Page infographics
+ *
+ */
+get_header();
+?>
+
+		<div class="stm_titlebox stm_titlebox_style_2 stm_titlebox_text-center">
+			<div class="container">
+				<div class="stm_flex stm_flex_last stm_flex_center">
+					<div class="stm_titlebox__inner">
+						<h1 class="h1 stm_titlebox__title no_line text-transform stm_mgb_2"><?php echo get_the_title();?></h1>					
+					</div>
+				</div>
+			</div>
+		</div>
+<?php
+
 
 $postslistinfografika_on_top = get_posts( [
 	'posts_per_page' => 100,
@@ -127,32 +145,26 @@ foreach( $postslistdocument as $post ){
 				</div>
 				<div class="custom-grid-1-4 photo-gallery">
 					<?php
-					if (!empty($postslistinfografika_on_top)) :
-						foreach( $postslistinfografika_on_top as $post ):
-							setup_postdata($post);
-							if(has_post_thumbnail()):
+					$terms = array(
+						'taxonomy' => 'infographic-cat',
+						'hide_empty' => false,
+						// 'child_of' => 25,							
+					);
+
+					if (!empty($terms)) :
+						foreach( $terms as $term ) :
+							var_dump($term);
 								?>
 								<div>
-								 <?php 
-										if( '' !== $post->post_content ) { ?>
-											<div class="video-21-9-rate-wrapepr img_url ">
-												<a data-toggle="modal" data-target="#modal-<?php the_ID() ?>" style="display: flex;justify-content: center; position: relative;">
-													<img src="<?php echo get_template_directory_uri() . '/assets/images/play_icon.png' ?>" class="play_icon">
-													<img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid">
-												</a>
-												<?php the_field('video_link'); ?>
-											</div>
-											<h5 class="custom-post-title"><?php the_title(); ?></h5>
-										<?php }else{ ?>
-											<div class="video-21-9-rate-wrapepr img_url ">
-											<a class="href_img" href="<?php echo get_the_post_thumbnail_url(); ?>" data-title="<?php the_title(); ?>" data-gallery="infographics"
-												data-toggle="lightbox" data-target=".bd-example-modal-lg">
-												<img src="<?php echo the_post_thumbnail_url() ?>" class="video-21-9-rate-images">
-											</a>
-											</div>
-											<h5 class="custom-post-title"><?php the_title(); ?></h5>
-									<?php	}
-									?>
+										
+									<div class="video-21-9-rate-wrapepr img_url ">
+									<a class="href_img" href="<?php  ?>" data-title="<?php the_title(); ?>" data-gallery="infographics"
+										data-toggle="lightbox" data-target=".bd-example-modal-lg">
+										<img src="<?php  echo z_taxonomy_image_url($term->term_id);   ?>" class="video-21-9-rate-images">
+									</a>
+									</div>
+									<h5 class="custom-post-title"><?php echo $term->name; ?></h5>
+								
 								</div>
 									<div class="modal fade custom-modal" id="modal-<?php the_ID() ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
 									 <div class="modal-dialog modal-dialog-centered" role="document">
@@ -171,57 +183,7 @@ foreach( $postslistdocument as $post ){
 									</div>
 								</div>
 							<?php
-							endif;
 						endforeach;
-						wp_reset_postdata();
-					endif;
-					if (!empty($postslistinfografika)):
-						foreach( $postslistinfografika as $post ):
-							setup_postdata($post);
-							if(has_post_thumbnail()):
-								?>
-								<div>
-								 <?php 
-										if( '' !== $post->post_content ) { ?>
-											<div class="video-21-9-rate-wrapepr img_url ">
-												<a data-toggle="modal" data-target="#modal-<?php the_ID() ?>" style="display: flex;justify-content: center; position: relative;">
-													<img src="<?php echo get_template_directory_uri() . '/assets/images/play_icon.png' ?>" class="play_icon">
-													<img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid">
-												</a>
-												<?php the_field('video_link'); ?>
-											</div>
-											<h5 class="custom-post-title"><?php the_title(); ?></h5>
-										<?php }else{ ?>
-											<div class="video-21-9-rate-wrapepr img_url ">
-											<a class="href_img" href="<?php echo get_the_post_thumbnail_url(); ?>" data-title="<?php the_title(); ?>" data-gallery="infographics"
-												data-toggle="lightbox" data-target=".bd-example-modal-lg">
-												<img src="<?php echo the_post_thumbnail_url() ?>" class="video-21-9-rate-images">
-											</a>
-											</div>
-											<h5 class="custom-post-title"><?php the_title(); ?></h5>
-									<?php	}
-									?>
-								</div>
-									<div class="modal fade custom-modal" id="modal-<?php the_ID() ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-									 <div class="modal-dialog modal-dialog-centered" role="document">
-										<div class="modal-content">
-											<div class="modal-header">
-												
-												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-												<span aria-hidden="true">&times;</span>
-												</button>
-												<h5 class="modal-title" id="exampleModalLongTitle"><?php the_title(); ?></h5>
-											</div>
-											<div class="modal-body">
-												<?php the_content(); ?>
-											</div>
-										</div>
-									</div>
-								</div>
-							<?php
-							endif;
-						endforeach;
-						wp_reset_postdata();
 					endif;
 					
 					?>
@@ -312,3 +274,6 @@ foreach( $postslistdocument as $post ){
 		</div>
 	</div>
 </section> 
+
+<?php
+get_footer();
